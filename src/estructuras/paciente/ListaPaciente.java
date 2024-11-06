b7098fc206edf73c106e4eaa5356862dfc6791d9
Sergio <111243440+sergioantonioar@users.noreply.github.com>
2024-11-06 16:03:38 -0500

    Add pacientes and packages

---
+++ src/estructuras/paciente/ListaPaciente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras.paciente;
+
+/**
+ *
+ * @author Sergio
+ */
+public class ListaPaciente {
+    
+}
