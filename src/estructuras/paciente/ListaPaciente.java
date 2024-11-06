35278d19a8fa1660ac3e2059bc12ee4ba65e7b84
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2024-11-06 16:30:56 -0500

    divergent solution file

--- src/estructuras/paciente/ListaPaciente.java
+++ src/estructuras/paciente/ListaPaciente.java
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
->>>>>>> b7098fc206edf73c106e4eaa5356862dfc6791d9
+
 package estructuras.paciente;
 
 /**
