1bee3899c3a350ec8d759cdffa18bf33359c64cc
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2024-11-07 20:41:52 -0500

    add persistenciaPaciente

--- src/persistencia/AlmacenarPaciente.java
+++ src/persistencia/AlmacenarPaciente.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package persistencia;
 
-/**
- *
- * @author Sergio
- */
+import estructuras.paciente.ListacPaciente;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import procesos.Mensajes;
+
 public class AlmacenarPaciente {
+    public static String ARCHIVO = "DatosListaPacientes.bin";
+    public static void guardarLista(ListacPaciente Lista){
+        try {
+            FileOutputStream fos = new FileOutputStream(ARCHIVO);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(Lista);
+            oos.close();
+        } catch (Exception e) {
+            Mensajes.leerTexto("ERROR no se puede guardar "+e);
+        }
+    }
     
+    public static ListacPaciente recuperarLista(){
+        ListacPaciente Lista = new ListacPaciente();
+        try {
+            FileInputStream fis = new FileInputStream(ARCHIVO);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Lista = (ListacPaciente) ois.readObject();
+        } catch (Exception e) {
+            Mensajes.mostrar("ERROR no se puede recuperar... "+e);
+        }
+        return Lista;
+    }
 }
